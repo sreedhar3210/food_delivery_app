28888535f445c5039a24d577fe12ea7aa8085632
sreedhar3210 <sreedhar.palampalli@gmail.com>
2023-10-10 22:51:50 +0530

    basic setup is completed

---
+++ mernapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
