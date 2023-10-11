0b6ffe0ad3a253a2317ebf158d5ad64f1d00bc04
sreedhar3210 <sreedhar.palampalli@gmail.com>
2023-10-11 23:17:10 +0530

    React Router is setup correctly

--- README.md
+++
@@ -1,2 +0,0 @@
-# food_delivery_app
-Our food delivery app combines easy ordering, secure authorization, and personalized food suggestions. Using the OpenAI API, we inject humor into the users’ experience, providing not just a treat but a memorable moment. Have a convenient, safe and fun experience with us.
--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Home from './screens/Home';
+import Login from './screens/Login';
+
+import{
+  BrowserRouter,
+  Routes,
+  Route,              //used for specifying path and jsx file to be rendered at that path
+}from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <div>
-        <Home/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/login" element={<Login/>}></Route>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
