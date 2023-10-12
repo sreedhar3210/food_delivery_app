835b3631c2a3427a46ed5dde538325aef4e6bd29
sreedhar3210 <sreedhar.palampalli@gmail.com>
2023-10-12 09:39:51 +0530

    Card is created as a seperate object

---
+++ mernapp/src/components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div>
+            <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
+                <img className="card-img-top" src="..." alt="image-cap" />
+                {/* alt is used if image is failed to load --*/}
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Checking if card is working.</p>
+                    <div className="container w-100">
+                        <select className='m-2 h-100 bg-success'>
+                            {(() => {
+                                let options = [];
+                                for (let i = 1; i <= 6; i++) {
+                                    options.push(<option key={i} value={i}> {i} </option>);
+                                }
+                                return options;
+                            })()}{/* "()=>{}"  is a function now we have made this function a block using () i.e 
+                                "(()=>{})" is a functional block now to call any function we should call it using '()'
+                                so the final structure has become like "(()=>{}()"
+                                this concept is called immediate invoked function expressions (IIEF)*/}
+                        </select>
+                        <select className='m-2 h-100 bg-success rounded'>
+                            <option key="half">Half</option>
+                            <option key="full">Full</option>
+                        </select>
+                        <div className='d-inline h-100 fs-5'>Total Price</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
 
 export default function Home() {
   return (
     <div>
-      <div> <Navbar/> </div>
-      <div>
-        <div className="card mt-3" style={{"width": "18rem", "maxHeight": "360px"}}>
-          <img className="card-img-top" src="..." alt="image-cap"/>
-          {/* alt is used if image is failed to load --*/}
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Checking if card is working.</p>
-            <div className="container w-100">
-              <select className='m-2 h-100 w-100 bg-success'></select>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div> <Footer/> </div>
+      <div> <Navbar /> </div>
+      <div> <Card /> </div>
+      <div> <Footer /> </div>
     </div>
   )
 }
